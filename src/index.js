016494cb371b03bc8e015eb3d2597f3be047f765
Jasmin Bissig <jasmin.bissig@gmail.com>
2018-11-17 22:17:24 +0100

    Cancel project SASS, remove files and folders again

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/index.css'
 import App from './components/App';
-
-import './styles/css/index.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
