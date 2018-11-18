39eba9a1b6ada978d3315b9cd969156782057303
Jasmin Bissig <jasmin.bissig@gmail.com>
2018-11-18 12:47:20 +0100

    Imported and defined PropTypes to Recipe.js in order to check the type of data passed from App to Recipe

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Recipe extends Component {
     
 }
 
 
+Recipe.PropTypes = {
+    ingredients: PropTypes.array.isRequired,
+    steps: PropTypes.array.isRequired,
+    title: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired
+};
 
 export default Recipe;
